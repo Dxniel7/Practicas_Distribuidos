cf5801d8d6948e11818b4ec1772d9e9482a50087
Daniel Hernandez <dxniel76@outlook.com>
2025-06-24 23:10:17 -0600

    Eliminar archivos ignorados del historial

--- Servicio_Web_Nube/src/main/java/com/panaderia/repository/PanRepositorio.java
+++
@@ -1,8 +0,0 @@
-package com.panaderia.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.panaderia.model.Pan;
-
-public interface PanRepositorio extends JpaRepository<Pan, Long> {
-    Pan findByProducto(String producto);
-}
